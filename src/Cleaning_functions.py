4ba40b881414c73e24b393a31c0d804eb2e1fd80
Maia Nahmod <maia.nahmod@gmail.com>
2022-10-24 13:02:50 +0200

    update data cleaning

--- src/Cleaning_functions.py
+++ src/Cleaning_functions.py
@@ -20,8 +20,8 @@ import random as rd
 
 def correct_Annual_Premium(dataframe):
     """
-    Receive the dataframe and replace in the gender column 1 with m and 2 with f.
-    
+    Receive the dataframe and replace the Annual_Premium variable with the log of the feature. From the distribution 
+    we choose the data with less dispersion. 
     Parameters
     ----------
     dataframe: pandas.core.frame.DataFrame
@@ -33,9 +33,17 @@ def correct_Annual_Premium(dataframe):
     """
     
     dataframe['log_premium'] = np.log(dataframe['Annual_Premium'])
-    data_mask = dataframe['log_premium'] > 9
+    data_mask = dataframe['log_premium'] > dataframe['log_premium'].median()
     dataframe = dataframe[data_mask]
     
     
+    return dataframe
+
+
+# In[3]:
+
+
+def correct_policy(dataframe):
+    dataframe['Policy_Sales_Channel'] = np.where((dataframe['Policy_Sales_Channel'] != 152.0) & (dataframe['Policy_Sales_Channel'] != 26.0) & (dataframe['Policy_Sales_Channel'] != 160.0) & (dataframe['Policy_Sales_Channel'] != 122.0), 'other', dataframe['Policy_Sales_Channel'])
     return dataframe
 
